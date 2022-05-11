a5ebe46339f690b4f37693bbbd3486c5d45500c6
Halaiksse <alexandrebb691@gmail.com>
2022-05-11 09:26:15 -0400

    Remove & Show + bug fixes
    Added a remove UUID command and Show the user list, bug fixes such as verification before adding a UUID to the list

--- discordimplementation.py
+++ discordimplementation.py
@@ -5,7 +5,6 @@ from discord.ext import commands # This is just an extension to make commands a 
 import requests
 import math
 import random
-import json
 
 
 bot = commands.Bot(command_prefix=".", help_command=None)
@@ -23,12 +22,14 @@ async def isOnline(ctx):
 async def Help(ctx):
     embed1=discord.Embed(
         title="Help Page",
-        color=discord.Color.blue())
+        color=discord.Color.dark_gold())
    
     embed1.add_field(name=".isOnline", value="Sends a message if the bot is online.", inline=False)
     embed1.add_field(name=".Help", value="I think you can figure out this one by yourself.", inline=False)
     embed1.add_field(name=".under25k", value="Returns a list of non exempted members under 25k", inline=False)
     embed1.add_field(name=".ListAdd", value="Adds the UUID of a player to the GXP exemption list", inline=False)
+    embed1.add_field(name=".ListRemove", value="Remove the UUID of a player from the GXP exemption list", inline=False)
+    embed1.add_field(name=".ListShow", value="Shows the GXP exemption list", inline=False)
     await ctx.send(embed=embed1)
 
 @bot.command()
@@ -46,7 +47,10 @@ async def under25k(ctx):
         uname = x['data']['player']['username']
         expHistory = expHistory = g['guild']['members'][i]['expHistory']
         expHistory = sum(expHistory.values())
-        if (int(expHistory) >= 0 and int(expHistory) < 25000):
+        player_rank = g['guild']['members'][i]['rank']
+        ExemptList = ["Officer", "Manager", "Guild Master"]
+
+        if (int(expHistory) >= 0 and int(expHistory) < 25000 and player_rank not in ExemptList):
             UnderReqList.append(uname)
             GxpList.append(expHistory)
     #Send msg         
@@ -79,12 +83,25 @@ async def under25k(ctx):
 @bot.command()
 async def ListAdd(ctx, P_UUID):
     ErrorFind = False
+    #Check if format is valid
+    if (len(P_UUID)!=32):
+        await ctx.send("UUID Format incorrect, try to enter the raw/trimmed UUID")
+        ErrorFind = True
+    #check if uuid exists    
     x = requests.get("https://playerdb.co/api/player/minecraft/" + P_UUID)
     x = x.json()
-    if x.get('error') == True:
+    if (x.get('error') == True and ErrorFind == False):
         await ctx.send("I was unable to find the player.")
         ErrorFind = True
-    
+    #check if player exists
+    InitList = open("safelist.txt", "r")
+    currentplayers = InitList.readlines()
+    InitList.close()
+    for line in currentplayers:
+        if line.strip("\n") == str(P_UUID):
+            ErrorFind = True
+            await ctx.send("Player is already in the list")
+
     #add to txt
     if (ErrorFind == False):
         PeopleTXT = open('safelist.txt', 'a')
@@ -92,17 +109,63 @@ async def ListAdd(ctx, P_UUID):
         PeopleTXT.close
         await ctx.send("The player has been added to the list")
 
-    
+@bot.command()
+async def ListRemove(ctx, P_UUID):
+    IGNFound = False
+
+    InitList = open("safelist.txt", "r")
+    currentplayers = InitList.readlines()
+    InitList.close()
+
+    Safelist = open("safelist.txt", "w")
+    for line in currentplayers:
+        if line.strip("\n") != str(P_UUID):
+            Safelist.write(line)
+        if line.strip("\n") == str(P_UUID):
+            IGNFound = True
+    Safelist.close()
+    if IGNFound:
+        await ctx.send("The player has been removed from the list")
+    else:
+        await ctx.send("Could not find the player in the list")
+
 
 # Local file containing the API key
 APILIST = []
 for line in open("c:\\python\\API.txt", "r").readlines():
     APILIST.append(line.strip())
 
+@bot.command()
+async def ListShow(ctx):
+    j = 0
+    SpecialExempts = []
+    for line in open ('safelist.txt', 'r').readlines():
+        SpecialExempts.append(line.strip())
+    
+    embedlist=discord.Embed(
+        title="Safelisted users",
+        color=discord.Color.dark_gold()
+        ) 
+    for member in SpecialExempts:
+        j+=1
+        x = requests.get("https://playerdb.co/api/player/minecraft/" + member)
+        x = x.json()
+        uname = x['data']['player']['username']
+
+        
+        if (j % 25 != 0 and j<25): # Add check Hono/Insurance
+            embedlist.add_field(name=member, value="**➤ IGN: ** `"+ uname +"`", inline=False)
+        else:
+            if (j % 25 == 0):
+                await ctx.send(embed=embedlist)
+                embedlist=discord.Embed(
+                    title="Safelisted users",
+                    color=discord.Color.dark_gold()
+                ) 
+        if (j > 25 and (j % 25 !=0)): #Add check Hono/Insurance
+            embedlist.add_field(name=member, value="**➤ IGN: ** `"+ uname +"`", inline=False)    
+    await ctx.send(embed=embedlist)
 
 
 # This just runs our bot using a local file containing the API key.
 bot.run(APILIST[0]) 
-
-#TODO LIST :
-#COMMAND TO REMOVE POEPLE FROM LIST
